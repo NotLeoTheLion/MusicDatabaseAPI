2d1c491664434c032e67935e9abe18f745c69c34
s047194 <158143159+s047194@users.noreply.github.com>
2024-06-18 00:52:24 +0300

    Delete MusicAPI/src/main/java/lt/viko/eif/groupproject/musicapi/repository directory

--- MusicAPI/src/main/java/lt/viko/eif/groupproject/musicapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.viko.eif.groupproject.musicapi.repository;
-
-import lt.viko.eif.groupproject.musicapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    boolean existsByUsername(String username);
-}
